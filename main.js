1abdb7eb9d3264568439e890904bcffa5a605a46
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-04-19 18:34:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+if (condition) {
+    
+}
+
+q2 = "I look at you, you look at me, I raise my right, you raise your left. What am I?"
+answers_2_1 = "River"
+answers_2_2 = "Humans"
+answers_2_3 = "Clock"
+answers_2_4 = "Mirror"
+
+q3 = "When will water stop running downhill?"
+answers_3_1 = "In the sea"
+answers_3_2 = "After the tap is shut off"
+answers_3_3 = "When water reaches the bottom of the hill"
+answers_3_4 = "After the rain stops"
+
+q4 = " What begins with an 'E' and contains only one letter?"
+answers_4_1 = "The letter 'E'"
+answers_4_2 = "Echo"
+answers_4_3 = "Envolope"
+answers_4_4 = "Email"
+
+q5 = "What can 't talk but will reply when spoken to?"
+answers_5_1 = "Mirror"
+answers_5_2 = "Cliff"
+answers_5_3 = "Echo"
+answers_5_4 = "Cave"
+
+
+//$('nextq').each(function () {
+  //  var counter = 0;
+
+    //$(this).click(function () {
+      //  counter++;
+        //alert(this.innerText + ' has been clicked ' + counter + ' times');
+    //});
+//})
+
+//var count = 0;
+//function nextquestion() {
+    //document.getElementById("demo").innerHTML = count++;
+  //  $("button#special").on('click', function () {
+    //    count++;
+      //  $("demo").text(count);
+    //});
+
+    //$("demo").text(count);
+//}
+
+
+
+
+
+//function nextquestion() {
+  //  document.getElementById("op1").innerHTML = ""
+//I look at you, you look at me, I raise my right, you raise your left. What am I?/
+//}
+
+
+//function nextquestion() {
+//    document.getElementById("questions").innerHTML = q2
+//}
+//q2 = "<input type='radio' id='leaf' name='option' value='leaf'> <label id='op1' for='leaf'>Leaf</label>"
+//<label id="op1" for="leaf">Leaf</label> 
+//<input type="radio" id="towel" name="option" value="towel">
+//<label id="op2" for="towel"> Towel</label>
+//<input type="radio" id="fruit" name="option" value="fruit">
+//<label id="op3" for="fruit"> Fruit</label>
+//<input type="radio" id="buffalo" name="option" value="buffalo">
+//<label id="op4" for="buffalo"> Buffalo</label>
+
